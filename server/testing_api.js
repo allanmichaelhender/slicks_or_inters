6fe9ec7e7d8edcbd58344c0aaeabf86cd6006254
allanmichaelhender <allanhender@gmail.com>
2025-12-23 21:35:41 +0000

    setting up db pool

--- server/testing_api.js
+++ server/testing_api.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import pool from "./src/config/db.js";
+
+const getLatituideLongitude = async (race_name) => {
+  const text =
+    "SELECT latitude, longitude FROM races_data WHERE race_name = $1";
+  const values = [race_name];
+
+  const { rows } = await pool.query(text, values);
+  return rows[0];
+};
+
+const { latitude, longitude } = await getLatituideLongitude(
+  "Australian Grand Prix"
+);
 
 const options = {
   method: "GET",
-  url: "https://api.tomorrow.io/v4/weather/forecast?location=-37.1,144.1&timestamps=daily&units=metric&apikey=iGWNWGAus1qI5KH1CGYJKQBToslmTYom",
+  url: `https://api.tomorrow.io/v4/weather/forecast?location=${latitude},${longitude}&timestamps=daily&units=metric&apikey=iGWNWGAus1qI5KH1CGYJKQBToslmTYom`,
   headers: {
     accept: "application/json",
     "accept-encoding": "deflate, gzip, br",
